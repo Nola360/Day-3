904b74d36dea51d67461a75c6d8074a8ca09c2e8
nola360 <adaramolajr@gmail.com>
2020-01-03 23:58:22 -0500

    added arrays and mutating arrays content

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
-// Template Literals
+// Arrays
 
-const name = 'LeBron James';
-const jersey = 23;
-const job = 'Professional BasketBall';
-const city = "Los Angeles";
-let html;
+const numbers = [42, 23, 33, 24, 6, 3, 2, 11, 22, 21];
+const numbers2 = new Array(22, 33, 44, 55, 66);
+const fruit = ['apple', 'orange', 'pear', 'pineapple'];
+const mixed = [22, 'Hello', true, undefined, null, { a: 1, b: 1 }, new Date()];
 
-// With template strings (es6)
+console.log(numbers);
+console.log(numbers2);
+console.log(fruit);
+console.log(mixed);
 
-function hello() {
-  return 'hello';
-}
+let val;
 
-html = `
-<ul>
-  <li>Name: ${name}</li>
-  <li>Jersey: ${jersey}</li>
-  <li>Job: ${job}</li>
-  <li>City: ${city}</li>
+// Get Array length
+val = numbers.length;
+console.log(val);
+// Check if its an array
+val = Array.isArray(numbers);
+console.log(val);
+// Get single value
+val = numbers[3];
+console.log(val);
+
+val = numbers[5];
+console.log(val);
+
+// Insert into array
+numbers[2] = 100;
+console.log(val);
+
+// Find index of value
+val = numbers.indexOf(33);
+console.log(val);
 
 
-  // Expressions
-  <li>${hello()}</li>
-  <li>${2 + 2}</li>
-  <li>${jersey > 30 ? 'Over 30' : 'Under 30'}</li>
-</ul>;`
+// Mutating Arrays
+// Add on to end of array
+numbers.push(45);
+console.log(val);
 
-document.body.innerHTML = html;
-// console.log(html);
+// Add on to front
+fruit.unshift(200);
+console.log(fruit);
+
+// Take of end of array
+numbers2.pop();
+console.log(numbers2);
+
+// Take off from front
+numbers2.shift();
+console.log(numbers2);
+
+// Splice values
+numbers2.splice(1, 1);
+console.log(numbers2);
+
+// Reverse
+fruit.reverse();
+console.log(fruit);
